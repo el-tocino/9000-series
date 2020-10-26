1ca6f3d5b7da87e2585c7b730bc1e99bd5fd67f9
el-tocino <email+github@aforkinthetoad.com>
2020-10-26 03:08:04 -0500

    should work now.

--- statusscreen.py
+++ statusscreen.py
@@ -4,6 +4,7 @@ import random
 import string
 import time
 import pygame
+from pygame import freetype
 
 # Init pygame
 pygame.init()
@@ -12,22 +13,20 @@ screentraits = pygame.display.Info()
 wres = screentraits.current_w
 hres = screentraits.current_h
 screen = pygame.display.set_mode([wres, hres])
-white = [255, 255, 255]
-red = [130, 0, 0]
-brightred = [170, 1, 20]
-blue = [0, 0, 109]
-green = [14, 109, 14]
-yellow = [255, 250, 25]
-black = [0, 0, 0]
-burgundy = [88, 20, 32]
-navy = [0, 0, 153]
-purple = [83, 21, 122]
-darkblue = [0, 51, 102]
+white = [200, 200, 200]
+#red = '#950000'
+#brightred = '#aa0114'
+#blue = '#3333ff'
+#green = '#006600'
+#burgundy = '#800020'
+#navy = '#000099'
+#purple = '#993366'
+#darkblue = '#00008b'
 min_time = 5
 sysstrings = ('COM', 'VEH', 'NAV', 'GDE', 'CNT', 'NUC', 'ATM', 'HIB', 'DMG',
               'FLX', 'LIF', 'MEM', 'DMC')
-sysbgcolors = ('purple', 'darkblue', 'darkblue', 'navy', 'green', 'darkblue',
-               'red', 'green', 'brightred', 'navy', 'red', 'blue', 'burgundy')
+sysbgcolors = ('#993366', '#00008b', '#00008b', '#000099', '#006600', '#00008b',
+               '#950000', '#006600', '#aa0114', '#000099', '#950000', '#3333ff', '#800020')
 
 sysentry = 0
 
@@ -41,31 +40,29 @@ def sub_text():
     ftl2 = random.choice(string.hexdigits)
     stl1 = random.choice(string.hexdigits)
     stl2 = random.choice(string.hexdigits)
-    sub_string = tlb1 + tlb2 + tlb3 + ": " + ftl1 + ftl2 + " - " + stl1 + stl2
-    return sub_string
-
+    sub_string = tlb1 + tlb2 + tlb3 + ": " + ftl1 + ftl2 + " - " + stl1 + stl2
+    return str(sub_string)
 
 def set_bgcolor(bgcolor):
     """ fill the screen with a color. """
     pygame.display.flip()
-    screen.fill(bgcolor)
+    screen.fill(pygame.Color(bgcolor))
     pygame.display.update()
 
 def add_text(text_string):
     """ write the text to display. """
-    font = pygame.font.SysFont("moki", 128, bold=True)
-#    font = pygame.font.Font(None, 55)
-    text = font.render(text_string, True, text_color)
+    font = pygame.font.SysFont("moki", int(wres / 8), bold=True)
+    text = font.render(text_string, True, white)
     text_rect = text.get_rect(center=(int(wres / 2), int(hres / 2)))
     screen.blit(text, text_rect)
     pygame.display.update()
 
 def add_subtext():
     """ add sub-text to display. """
-    st = sub_text
-    font = pygame.font.SysFont("moki", 24, bold=True)
+    st = sub_text()
+    font = pygame.font.SysFont("moki", int(wres / 64), bold=True)
     text = font.render(st, True, white)
-    text_rect = text.get_rect(center=(int(wres / 2 - 100), int(hres / 2 - 100)))
+    text_rect = text.get_rect(center=(int(wres / 5), int(hres / 2 - (wres / 10))))
     screen.blit(text, text_rect)
     pygame.display.update()
 
@@ -75,7 +72,9 @@ while True:
         next_sys = sysentry + 1
     sysitem = sysstrings[next_sys]
     set_bgcolor(sysbgcolors[next_sys])
-    add_text(sysitem)
+    print(sysbgcolors[next_sys])
+    sep = " "
+    add_text(sep.join(sysitem))
     add_subtext()
     display_time = random.randint(0, 15) + min_time
     time.sleep(display_time)
